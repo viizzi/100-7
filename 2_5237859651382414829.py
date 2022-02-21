0d495983e5e936942f1295805492e849289035f5
viizzi <82059601+viizzi@users.noreply.github.com>
2022-02-21 23:59:24 +0200

    Пикап

---
+++ 2_5237859651382414829.py
@@ -0,0 +1,25 @@
+
+from .. import loader
+from random import choice as какаша
+
+
+def register(cb):
+	cb(ChatinfoMod())
+
+class PickaperMod(loader.Module):
+	"""Рандомная фраза пикапа."""
+	strings = {'name': 'Пикапер'}
+
+	async def pikapcmd(self, event):
+		'''Случайная фраза пикапа.'''
+		await event.edit('<b>\u2060</b>')
+		while True:
+			a=какаша(await event.client.get_messages('pikapr',1000))
+			if not a.entities:
+				await event.edit(f'<b>{a.text}</b>')
+				break 
+			else:
+				pass
+
+
+		
